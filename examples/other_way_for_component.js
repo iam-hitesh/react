2a111d070f929b890c89edc5b610dc950f4e29cd
Hitesh Yadav <hhiteshyadav@gmail.com>
2018-02-26 14:10:00 +0530

    Create other_way_for_component.js

---
+++ examples/other_way_for_component.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+// import speakers from './list'
+
+// function App(){
+//     return(
+//         <h2>Hello World</h2>
+//     );
+// }
+
+const App = (item,otherArgument) => {
+    return(
+        <h2>Hello World</h2>
+    )
+}
+export default App;
